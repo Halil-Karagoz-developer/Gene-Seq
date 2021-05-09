230fd9e3477c4b0cea065699e687a9b027d894fc
Halil Faruk KARAGOZ <karagozh18@itu.edu.tr>
2021-05-09 23:59:55 +0300

    site

---
+++ scr-2.js
@@ -0,0 +1,47 @@
+const inputTextbox = document.querySelector("#input--dna");
+const showData = document.querySelector("#show-data");
+const button_send = document.querySelector("#send-data");
+const key = ["F","L","I","M","V","S","P","T","A","Y","H","Q","N","K","D","E","C","W","R","G"]
+const val = ["UUU","CUG","AUC","AUG","GUC","UCG","CCG","ACC","GCG","UAC","CAU","CAG","AAC","AAG","GAU","GAG","UGC","UGG","CGC","GGC"]
+
+
+
+button_send.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(inputTextbox.value);
+    for(let i = 0; i < inputTextbox.value.length;i++){
+        for(let k = 0; k <key.length;k++){
+            if( key[k] == inputTextbox.value[i]){
+                showData.textContent += val[i] + " ";
+                
+            }
+        }
+    }
+    
+    console.log(typeof inputTextbox.value);
+    
+    // showData.append()
+})
+
+
+
+     
+     
+      
+   
+     
+     
+     
+     
+   
+    
+   
+   
+   
+    
+    
+    
+    
+   
+                                                    
+                                                    
